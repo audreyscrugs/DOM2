d9e341a185758a085af295d5511942e5c7c92d80
Jon Ewalt & Audrey Scrugham <student@epicodus.com>
2015-10-05 19:02:09 -0700

    Add initial JS functions to make pretty

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,18 @@
-$(function() {
+$(document).ready(function() {
 
   $("button#hello").click(function() {
-    $("ul").prepend("<li>Hello!</li>");
+      $("ul#user").prepend("<li>Hello!</li>");
+      $("ul#webpage").children("li").first().slideUp( 300 ).delay( 800 ).fadeIn( 400 ).prepend("<li>Why hello there!</li>");
   });
 
   $("button#goodbye").click(function() {
-    $("ul").prepend("<li>Goodbye!</li>");
+    $("ul#user").prepend("<li>Goodbye!</li>");
+    $("ul#webpage").prepend("<li>Goodbye, dear user!</li>")
   });
 
   $("button#stop").click(function() {
-    $("ul").prepend("<li>Stahp copying me!</li>");
+    $("ul#user").prepend("<li>Stop copying me!</li>");
+    $("ul#webpage").prepend("<li>Pardon me. I meant no offense.</li>")
   });
-  
+
 });
